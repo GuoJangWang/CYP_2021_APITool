8e5a5e945a450bf706d9c83ce155050ebc285616
國嶂 王 <lf2.net0919@gmail.com>
2020-10-28 13:41:16 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CYP_2021_APITool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
