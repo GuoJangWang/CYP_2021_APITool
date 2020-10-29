26617540a2ee39d27d1dbcc6cbbdb355bfde2357
國嶂 王 <lf2.net0919@gmail.com>
2020-10-29 11:02:24 +0800

    20201029_Microsoft.JSInterop

--- Views/Home/JavaScript.js
+++ Views/Home/JavaScript.js
@@ -1,1 +1,36 @@
-﻿
+﻿var IotApi = require('@arduino/arduino-iot-client');
+var rp = require('request-promise');
+
+async function getToken() {
+    var options = {
+        method: 'POST',
+        url: 'https://api2.arduino.cc/iot/v1/clients/token',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        json: true,
+        form: {
+            grant_type: 'client_credentials',
+            client_id: 'njCNbE7m6YEfTaWproQNK8ipV57A2ShR',
+            client_secret: 'Ev6K461zfEQ2DWMxG5Qw5GnKqv1jdewBTB7R5FXJZDr0IxkdCzT3HK3xLKPab6TU',
+            audience: 'https://api2.arduino.cc/iot'
+        }
+    };
+
+    try {
+        const response = await rp(options);
+        return response['access_token'];
+    }
+    catch (error) {
+        console.error("Failed getting an access token: " + error)
+    }
+}
+
+async function run() {
+    var client = IotApi.ApiClient.instance;
+    // Configure OAuth2 access token for authorization: oauth2
+    var oauth2 = client.authentications['oauth2'];
+    oauth2.accessToken = await getToken();
+
+
+}
+
+run();
