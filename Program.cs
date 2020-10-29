787db8dc11a2a8efa4d6dd79c33c7727b097f1ea
國嶂 王 <lf2.net0919@gmail.com>
2020-10-29 11:35:03 +0800

    20201029_SignalR

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 
 namespace CYP_2021_APITool
 {
