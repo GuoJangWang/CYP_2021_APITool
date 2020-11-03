0574b77d92354250c8d901f3e081cd7bcbc3f3f6
國嶂 王 <lf2.net0919@gmail.com>
2020-11-03 19:00:28 +0800

    20201103

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
+using System.Data.SqlClient;
 
 namespace CYP_2021_APITool
 {
@@ -23,5 +24,11 @@ namespace CYP_2021_APITool
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+        public void DB ()
+        {
+            
+        }
     }
 }
