be4b5b6a8e8f88e61fc57973d5bae0efaa2e858b
國嶂 王 <lf2.net0919@gmail.com>
2020-11-05 19:28:36 +0800

    20201105_insert_test

--- DB.cs
+++ DB.cs
@@ -10,32 +10,36 @@ namespace CYP_2021_APITool
     public class DB
     {
 
-        public static void DB_Connect()
+        public static string DB_Connect()
         {
-            String tagDB_connect_str = @"Data Source=IOT-IIS\SQLEXPRESS;Initial Catalog=CYP_Intouch;Persist Security Info=True;User ID=sa;Password=123456-s";
+            String tagDB_connect_str = @"Data Source=IOT-IIS\SQLEXPRESS;Initial Catalog=CYP_Intouch;Persist Security Info=True;User ID=Jason;Password=80138305jJ";
             SqlConnection SQLCC = new SqlConnection(tagDB_connect_str);
             SQLCC.Open();
 
             if (SQLCC.State == ConnectionState.Open)
             {
+                SqlTransaction t = SQLCC.BeginTransaction(); //BeginTransaction為SqlTransaction方法
+                SqlCommand cmd = new SqlCommand("INSERT INTO BarCode VALUES(20201105,'asdfg',true)", SQLCC, t);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    return "true";
+                }
+                catch
+                {
+                    return "fail";
+                }
                 
             }
+            else
+            {
+                return "fail";
+            }
 
+            
+       
 
-            //要連接資料庫的位置
-            //Initial Catalog = ArrangeSubjectDB   這是欲連接的資料庫
-            //server = (Local)                               server的位置
-
-
-
-            //SQL INSERT 語法
-            // INSERT INTO  [table] ([欄位名1],[欄位名2],[欄位名3],....) VALUES (@值1, @值2, @值3,...)
-            /*string strMsgInsert = @"INSERT INTO Arrangement 
-            ([teachID],[CS_ID],[classDate],[classSession],[classStartTime],[classEndTime]) 
-            VALUES(@value1, @value2, @value3,@value4, @value5, @value6) ";
-
-            //C# SQL Command 物件 SqlCommand( SQL語法 , SqlConnection )   
-            SqlCommand mySqlCmd = new SqlCommand();*/
         }
 
     }
